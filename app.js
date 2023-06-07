760b53781e003416426cea43b15cc3ee289389c7
madhwan <madhwanphadtere.123@gmail.com>
2023-06-07 15:53:04 +0530

    swagger documentation added

--- app.js
+++ app.js
@@ -2,15 +2,35 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const { connectToDatabase } = require("./db/mongoDb");
 const routes = require("./routes/routes");
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsdoc = require("swagger-jsdoc");
 
 const app = express();
 const port = process.env.port || 3001;
+
+// Swagger configuration options
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title: "Performance Report Management(Backend)",
+      version: "1.0.0",
+      description:
+        "The project is a Fresher's Training Portal that provides an API for managing and tracking the training progress and performance of interns. Interns can access their weekly ratings provided by their mentors, which reflect their performance in various factors such as Assessment, Attendance & Participation, Time Management, Communication Skills, Assignments, and Mentor's Feedback. The API endpoint allows interns to retrieve their weekly ratings, providing them with valuable insights into their progress and areas for improvement throughout their training program.",
+    },
+  },
+  apis: ["./routes/*.js"], // Path to the API routes files
+};
+
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
 
 // Parse JSON request bodies
 app.use(express.json());
 
 // Routes
 app.use("/", routes);
+
+// Serve Swagger API documentation
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // Connect to the database and start the server
 connectToDatabase()
--- routes/routes.js
+++ routes/routes.js
@@ -1,15 +1,41 @@
 const express = require("express");
-const { fetchReports, calculateFinalReports } = require("../controllers/reportController");
+const {
+  fetchReports,
+  calculateFinalReports,
+} = require("../controllers/reportController");
 
 const router = express.Router();
 
-// API endpoint to fetch reports for a specific user
+/**
+ * @swagger
+ * /report/{id}:
+ *   get:
+ *     summary: Fetch Performance Report Management of an intern by email ID.
+ *     description: Retrieve the Performance Report Management for a specific intern in the Fresher's Training Portal by providing their email ID as a parameter. This API endpoint fetches the relevant data and delivers it to the frontend for display and analysis.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Email ID of the intern.
+ *         schema:
+ *           type: string
+ *           format: email
+ *     responses:
+ *       200:
+ *         description: Success. Returns the Performance Report Management data.
+ *       400:
+ *         description: Bad request. Invalid email ID provided or intern not found.
+ *       401:
+ *         description: Unauthorized. User authentication required.
+ *       500:
+ *         description: Internal server error. Failed to fetch the Performance Report Management data.
+ */
+
 router.get("/report/:id", async (req, res) => {
   const userId = req.params.id;
   try {
     const { projectReports, weeklyReports } = await fetchReports(userId);
     const result = calculateFinalReports(projectReports, weeklyReports);
-    // console.log(JSON.stringify(result));
     res.json(result);
   } catch (error) {
     res.status(500).json({ error: "Error fetching project reports" });
